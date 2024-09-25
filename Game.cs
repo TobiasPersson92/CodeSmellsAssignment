bdc20a947c67db331694f063013a7301306291dd
gitTob <tobiaspersson1992@gmail.com>
2024-09-25 18:04:33 +0200

    everything should work

---
+++ Game.cs
@@ -0,0 +1,65 @@
+﻿using CodeSmellsAssignment.Common;
+using CodeSmellsAssignment.ConsoleIO;
+using CodeSmellsAssignment.Data;
+using CodeSmellsAssignment.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSmellsAssignment
+{
+    public class Game
+    {
+        private IConsoleInterface _consoleInterface;
+        private IGameLogic _gameLogic;
+        private IFileIO _fileIO;
+
+        public Game(IConsoleInterface consoleInterface, IGameLogic gameLogic, IFileIO fileIO)
+        {
+            _consoleInterface = consoleInterface;
+            _gameLogic = gameLogic;
+            _fileIO = fileIO;
+        }
+
+        public void Run()
+        {
+            HumanPlayer humanPlayer = new HumanPlayer();
+            humanPlayer.SetName(_consoleInterface.GetNameFromConsole());
+
+            bool gameIsRunning = true;
+
+            while (gameIsRunning)
+            {
+                string goalNumber = _gameLogic.CreateFourDigitNumberToGuess();
+
+                _consoleInterface.DisplayMessage("New game:\n");
+                _consoleInterface.DisplayMessage("For practice, number is: " + goalNumber + "\n"); //comment out or remove to play a real game!
+
+                string currentGuess;
+                string currentBullsAndCows;
+                do
+                {
+                    humanPlayer.UpdateTotalGuessesBy(1);
+                    currentGuess = _consoleInterface.GetUserInput();
+                    _consoleInterface.DisplayMessage(currentGuess + "\n");
+                    currentBullsAndCows = _gameLogic.ControlAmountOfBullsAndCows(goalNumber, currentGuess);
+                    _consoleInterface.DisplayMessage(currentBullsAndCows + "\n");
+                } while (currentBullsAndCows != "BBBB,");
+
+                _consoleInterface.ShowWinningMessage(humanPlayer.TotalGuesses);
+
+                _fileIO.SaveResultsToFile(humanPlayer.Name, humanPlayer.TotalGuesses);
+
+                List<PlayerData> topPlayers = _fileIO.LoadResultsFromFile();
+                _consoleInterface.DisplayPlayerData(topPlayers);
+
+                if (_consoleInterface.AskToContinue() == false)
+                {
+                    gameIsRunning = false;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CodeSmellsAssignment.ConsoleIO;
+using CodeSmellsAssignment.Data;
+using CodeSmellsAssignment.Logic;
+using CodeSmellsAssignment.Common;
+using static System.Net.WebRequestMethods;
+using Microsoft.Extensions.DependencyInjection;
+using CodeSmellsAssignment;
+
+namespace CleanCodeAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IConsoleInterface, ConsoleInterface>()
+                .AddSingleton<IGameLogic, GameLogic>()
+                .AddSingleton<IFileIO>(new FileIO("result.txt"))
+                .AddSingleton<Game>()
+                .BuildServiceProvider();
+
+            var game = serviceProvider.GetService<Game>();
+            game.Run();
+
+        }
+    }
+}
